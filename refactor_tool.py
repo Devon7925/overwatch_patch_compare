2aa42a66ffb1fddfff5d29e556f8fc11afcb8423
Devon7925 <devonrutledge03@gmail.com>
2024-07-18 22:14:46 -0600

    added july 11 2023 patch

--- refactor_tool.py
+++ refactor_tool.py
@@ -45,6 +45,6 @@ def process_json_files(folder_path, path, new_key, new_value=None, rename=False)
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Reload time"
+new_property_name = "Time to wear off the target when not in line-of-sight"
 new_value = 1.5
-process_json_files(folder_path, 'heroes/support/Ana/abilities/Biotic Rifle', new_property_name, new_value, rename=False)
+process_json_files(folder_path, 'heroes/support/Zenyatta/abilities/Orb of Discord', new_property_name, new_value, rename=False)
