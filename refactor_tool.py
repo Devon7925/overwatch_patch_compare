51205e2f57e678af8076ebb94c3ab778687345cb
Devon7925 <devonrutledge03@gmail.com>
2024-07-20 20:06:21 -0600

    Added APRIL 25, 2023 patch

--- refactor_tool.py
+++ refactor_tool.py
@@ -45,6 +45,6 @@ def process_json_files(folder_path, path, new_key, new_value=None, rename=False)
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Cooldown"
-new_value = 6
-process_json_files(folder_path, 'heroes/tank/Orisa/abilities/Energy Javelin', new_property_name, new_value, rename=False)
+new_property_name = "Delay until slow"
+new_value = 1
+process_json_files(folder_path, 'heroes/support/Lifeweaver/abilities/Healing Blossom', new_property_name, new_value, rename=False)
