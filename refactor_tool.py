c8d21d17996a95b29bb5e66695be3fd3ee07ade6
Devon7925 <devonrutledge03@gmail.com>
2024-07-28 16:33:49 -0600

    Added JULY 14TH, 2022 beta patch

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Invulnerability duration"
-new_value = 0.25
-process_json_files(folder_path, 'heroes/support/Kiriko/abilities/Swift Step/Invulnerability duration', new_property_name, new_value, rename=False)
+new_property_name = "Cooldown"
+new_value = 12
+process_json_files(folder_path, 'heroes/damage/Symmetra/abilities/Teleporter/Cooldown', new_property_name, new_value, rename=False)
