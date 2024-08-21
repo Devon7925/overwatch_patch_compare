da28722f2e32bfff7d41bff2577aa5608bd371cd
Devon7925 <devonrutledge03@gmail.com>
2024-08-20 19:14:06 -0600

    Added queen patch

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "heroes/damage/Torbjörn/abilities/Forge Hammer/Recovery time"
-new_value = {}
-process_json_files(folder_path, 'heroes/damage/Torbjörn/abilities/Forge Hammer/Swing recovery time', new_property_name, new_value, rename=True)
+new_property_name = "Movement speed bonus"
+new_value = 30
+process_json_files(folder_path, 'heroes/tank/Junker Queen/abilities/Commanding Shout/Movement speed bonus', new_property_name, new_value, rename=False)
