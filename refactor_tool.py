24cdcb2a647a922e54b07579c74c12c2d598c8fa
Devon7925 <devonrutledge03@gmail.com>
2024-08-23 11:03:44 -0600

    Added genji alt fire

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Ammo per shot"
-new_value = 5
-process_json_files(folder_path, 'heroes/damage/Widowmaker/abilities/Widow\'s Kiss Alt Fire/Ammo per shot', new_property_name, new_value, rename=False)
+new_property_name = "Pellet count"
+new_value = 3
+process_json_files(folder_path, 'heroes/damage/Genji/abilities/Shuriken Alt Fire/Pellet count', new_property_name, new_value, rename=False)
