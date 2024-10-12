792ec30f8b2665b5e0cb7e610fe054fa92454b99
Devon7925 <devonrutledge03@gmail.com>
2024-10-12 12:44:11 -0600

    data fixes

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Charge damage"
-new_value = 120
-process_json_files(folder_path, 'heroes/damage/Ashe/abilities/B.O.B/Charge damage', new_property_name, new_value, rename=False)
+new_property_name = "Hack damage multiplier"
+new_value = 2
+process_json_files(folder_path, 'heroes/damage/Sombra/abilities/Virus/Hack damage multiplier', new_property_name, new_value, rename=False)
