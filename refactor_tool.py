e2297d6455d97a79ad330708262b6cef5ab3b768
Devon7925 <devonrutledge03@gmail.com>
2024-12-18 13:41:15 -0700

    added overwatch 6v6

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Explosion radius"
-new_value = 8
-process_json_files(folder_path, 'heroes/support/Illari/abilities/Captive Sun/Explosion radius', new_property_name, new_value, rename=False)
+new_property_name = "Healing per projectile"
+new_value = 13
+process_json_files(folder_path, 'heroes/support/Kiriko/abilities/Healing Ofuda/Healing per projectile', new_property_name, new_value, rename=False)
