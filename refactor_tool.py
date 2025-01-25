980b1e626f886a4017d1004e9b01efca211e0588
Devon7925 <devonrutledge03@gmail.com>
2025-01-24 22:40:34 -0700

    added 2 overwatch 1 patches

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Base damage per second"
-new_value = 50
-process_json_files(folder_path, 'heroes/damage/Echo/abilities/Focusing Beam/Base damage per second', new_property_name, new_value, rename=False)
+new_property_name = "Minimum health when exiting Duplicate"
+new_value = 100
+process_json_files(folder_path, 'heroes/damage/Echo/abilities/Duplicate/Minimum health when exiting Duplicate', new_property_name, new_value, rename=False)
--- unadded changes.md
+++ unadded changes.md
@@ -1,5 +1,6 @@
 * Sojourn `Secondary fire damage now scales linearly with energy from 30 to 130 damage (1 energy converts to 1 damage added)` (how tf did it work before)
 * Pharah `Reload starts 0.25 seconds sooner when out of ammo` What tf? is there a delay on reload when out of ammo,
 * Junkrat `Projectiles preserve slightly more velocity on ricochet.` How much more? How much was there before?
+* Zarya `Beam damage now scales from 75-170 damage-per-second, down from 95-170` minimum damage was later increased from 85 so I'm not sure when it was increased to 85 from 75
 
 
