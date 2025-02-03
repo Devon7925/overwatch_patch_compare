2142aca7f5b9f16cac0dcb1bdb83acc51575022b
Devon7925 <devonrutledge03@gmail.com>
2025-02-03 01:08:36 -0700

    Last 2020 patches added

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "Flight speed boost"
-new_value = 30
-process_json_files(folder_path, 'heroes/tank/Sigma/abilities/Gravitic Flux/Flight speed boost', new_property_name, new_value, rename=False)
+new_property_name = "Recovery time"
+new_value = 0.35
+process_json_files(folder_path, 'heroes/damage/Doomfist/abilities/Rising Uppercut/Recovery time', new_property_name, new_value, rename=False)
