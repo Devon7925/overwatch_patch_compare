3c19b2a1a37eed0321f630bdc73f43b66d5b7191
Devon7925 <devonrutledge03@gmail.com>
2025-03-25 16:57:56 -0600

    Added march 25th patch

--- refactor_tool.py
+++ refactor_tool.py
@@ -50,6 +50,6 @@ def process_json_files(folder_path, old_path, new_path=None, new_value=None, ren
 folder_path = 'patches'
 
 # Example usage for adding
-new_property_name = "weapon recharging paused while using quick melee"
-new_value = True
-process_json_files(folder_path, 'heroes/Illari/abilities/Solar Rifle/weapon recharging paused while using quick melee', new_property_name, new_value, rename=False)
+new_property_name = "Duration"
+new_value = 8
+process_json_files(folder_path, 'heroes/Ana/perks/Shrike/Duration', new_property_name, new_value, rename=False)
